3a0221babf94b6aff095051f06201027ebd2cebf
jdvpl <juanda554242@gmail.com>
2021-09-02 22:19:00 -0500

    app.user si es exitoso envia un texto al front

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # NodeJs-REST-APIs---Projects-Express-Swagger-MySQL-
 
+
+### ejecutar siempre el comando npm i para probarlo localmente
+`npm i`
 ### pasos de instalacion
 
 `npm init`
 
 ### instalar express
 `npm i express`
+
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const app = express();
 
+
+app.use('/test', (req, res)=>{
+    console.log('received request');
+    res.status(200).send("exitoso");
+})
 app.listen(3000, ()=>{
     console.log('Estoy escuchando el puerto');
 })
